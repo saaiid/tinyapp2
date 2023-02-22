b51511d5725b606fc86b13cf11f9120dec133e48
saaiid <ibrosakid@gmail.com>
2023-02-21 19:57:33 -0500

    added a url pages

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: req.params.urls /* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
